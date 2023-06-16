2d8091cb9f135bc3f4b0b2c4b98bde2c6ba07927
AlessandroMarc <52158784+AlessandroMarc@users.noreply.github.com>
2023-06-16 12:59:56 +0200

    Reformatted

--- 5.CLI/lib/cli.js
+++ 5.CLI/lib/cli.js
@@ -8,7 +8,7 @@ var readline = require('readline');
 var util = require('util');
 var debug = util.debuglog('cli');
 var events = require('events');
-class _events extends events {};
+class _events extends events { };
 var e = new _events();
 var os = require('os');
 var v8 = require('v8');
@@ -20,43 +20,43 @@ var helpers = require('./helpers');
 var cli = {};
 
 // Input handlers
-e.on('man', function(str) {
+e.on('man', function (str) {
   cli.responders.help();
 });
 
-e.on('help', function(str) {
+e.on('help', function (str) {
   cli.responders.help();
 });
 
-e.on('exit', function(str) {
+e.on('exit', function (str) {
   cli.responders.exit();
 });
 
-e.on('stats', function(str) {
+e.on('stats', function (str) {
   cli.responders.stats();
 });
 
-e.on('list users', function(str) {
+e.on('list users', function (str) {
   cli.responders.listUsers();
 });
 
-e.on('more user info', function(str) {
+e.on('more user info', function (str) {
   cli.responders.moreUserInfo(str);
 });
 
-e.on('list checks', function(str) {
+e.on('list checks', function (str) {
   cli.responders.listChecks(str);
 });
 
-e.on('more check info', function(str) {
+e.on('more check info', function (str) {
   cli.responders.moreCheckInfo(str);
 });
 
-e.on('list logs', function(str) {
+e.on('list logs', function (str) {
   cli.responders.listLogs();
 });
 
-e.on('more log info', function(str) {
+e.on('more log info', function (str) {
   cli.responders.moreLogInfo(str);
 });
 
@@ -64,8 +64,52 @@ e.on('more log info', function(str) {
 // Responders object
 cli.responders = {};
 
+
+// Create a vertical space
+cli.verticalSpace = function (lines) {
+  lines = typeof (lines) == 'number' && lines > 0 ? lines : 1;
+  for (i = 0; i < lines; i++) {
+    console.log('');
+  }
+};
+
+// Create a horizontal line across the screen
+cli.horizontalLine = function () {
+
+  // Get the available screen size
+  var width = process.stdout.columns;
+
+  // Put in enough dashes to go across the screen
+  var line = '';
+  for (i = 0; i < width; i++) {
+    line += '-';
+  }
+  console.log(line);
+
+
+};
+
+// Create centered text on the screen
+cli.centered = function (str) {
+  str = typeof (str) == 'string' && str.trim().length > 0 ? str.trim() : '';
+
+  // Get the available screen size
+  var width = process.stdout.columns;
+
+  // Calculate the left padding there should be
+  var leftPadding = Math.floor((width - str.length) / 2);
+
+  // Put in left padded spaces before the string itself
+  var line = '';
+  for (i = 0; i < leftPadding; i++) {
+    line += ' ';
+  }
+  line += str;
+  console.log(line);
+};
+
 // Help / Man
-cli.responders.help = function() {
+cli.responders.help = function () {
 
   // Codify the commands and their explanations
   var commands = {
@@ -108,56 +152,13 @@ cli.responders.help = function() {
 
 };
 
-// Create a vertical space
-cli.verticalSpace = function(lines) {
-  lines = typeof(lines) == 'number' && lines > 0 ? lines : 1;
-  for (i = 0; i < lines; i++) {
-    console.log('');
-  }
-};
-
-// Create a horizontal line across the screen
-cli.horizontalLine = function() {
-
-  // Get the available screen size
-  var width = process.stdout.columns;
-
-  // Put in enough dashes to go across the screen
-  var line = '';
-  for (i = 0; i < width; i++) {
-    line += '-';
-  }
-  console.log(line);
-
-
-};
-
-// Create centered text on the screen
-cli.centered = function(str) {
-  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : '';
-
-  // Get the available screen size
-  var width = process.stdout.columns;
-
-  // Calculate the left padding there should be
-  var leftPadding = Math.floor((width - str.length) / 2);
-
-  // Put in left padded spaces before the string itself
-  var line = '';
-  for (i = 0; i < leftPadding; i++) {
-    line += ' ';
-  }
-  line += str;
-  console.log(line);
-};
-
 // Exit
-cli.responders.exit = function() {
+cli.responders.exit = function () {
   process.exit(0);
 };
 
 // Stats
-cli.responders.stats = function() {
+cli.responders.stats = function () {
   // Compile an object of stats
   var stats = {
     'Load Average': os.loadavg().join(' '),
@@ -198,15 +199,15 @@ cli.responders.stats = function() {
 };
 
 // List Users
-cli.responders.listUsers = function() {
-  _data.list('users', function(err, userIds) {
+cli.responders.listUsers = function () {
+  _data.list('users', function (err, userIds) {
     if (!err && userIds && userIds.length > 0) {
       cli.verticalSpace();
-      userIds.forEach(function(userId) {
-        _data.read('users', userId, function(err, userData) {
+      userIds.forEach(function (userId) {
+        _data.read('users', userId, function (err, userData) {
           if (!err && userData) {
             var line = 'Name: ' + userData.firstName + ' ' + userData.lastName + ' Phone: ' + userData.phone + ' Checks: ';
-            var numberOfChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array && userData.checks.length > 0 ? userData.checks.length : 0;
+            var numberOfChecks = typeof (userData.checks) == 'object' && userData.checks instanceof Array && userData.checks.length > 0 ? userData.checks.length : 0;
             line += numberOfChecks;
             console.log(line);
             cli.verticalSpace();
@@ -218,13 +219,13 @@ cli.responders.listUsers = function() {
 };
 
 // More user info
-cli.responders.moreUserInfo = function(str) {
+cli.responders.moreUserInfo = function (str) {
   // Get ID from string
   var arr = str.split('--');
-  var userId = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+  var userId = typeof (arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
   if (userId) {
     // Lookup the user
-    _data.read('users', userId, function(err, userData) {
+    _data.read('users', userId, function (err, userData) {
       if (!err && userData) {
         // Remove the hashed password
         delete userData.hashedPassword;
@@ -242,19 +243,19 @@ cli.responders.moreUserInfo = function(str) {
 };
 
 // List Checks
-cli.responders.listChecks = function(str) {
-  _data.list('checks', function(err, checkIds) {
+cli.responders.listChecks = function (str) {
+  _data.list('checks', function (err, checkIds) {
     if (!err && checkIds && checkIds.length > 0) {
       cli.verticalSpace();
-      checkIds.forEach(function(checkId) {
-        _data.read('checks', checkId, function(err, checkData) {
+      checkIds.forEach(function (checkId) {
+        _data.read('checks', checkId, function (err, checkData) {
           if (!err && checkData) {
             var includeCheck = false;
             var lowerString = str.toLowerCase();
             // Get the state, default to down
-            var state = typeof(checkData.state) == 'string' ? checkData.state : 'down';
+            var state = typeof (checkData.state) == 'string' ? checkData.state : 'down';
             // Get the state, default to unknown
-            var stateOrUnknown = typeof(checkData.state) == 'string' ? checkData.state : 'unknown';
+            var stateOrUnknown = typeof (checkData.state) == 'string' ? checkData.state : 'unknown';
             // If the user has specified that state, or hasn't specified any state
             if ((lowerString.indexOf('--' + state) > -1) || (lowerString.indexOf('--down') == -1 && lowerString.indexOf('--up') == -1)) {
               var line = 'ID: ' + checkData.id + ' ' + checkData.method.toUpperCase() + ' ' + checkData.protocol + '://' + checkData.url + ' State: ' + stateOrUnknown;
@@ -269,13 +270,13 @@ cli.responders.listChecks = function(str) {
 };
 
 // More check info
-cli.responders.moreCheckInfo = function(str) {
+cli.responders.moreCheckInfo = function (str) {
   // Get ID from string
   var arr = str.split('--');
-  var checkId = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+  var checkId = typeof (arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
   if (checkId) {
     // Lookup the user
-    _data.read('checks', checkId, function(err, checkData) {
+    _data.read('checks', checkId, function (err, checkData) {
       if (!err && checkData) {
 
         // Print their JSON object with text highlighting
@@ -290,11 +291,11 @@ cli.responders.moreCheckInfo = function(str) {
 };
 
 // List Logs
-cli.responders.listLogs = function() {
-  _logs.list(true, function(err, logFileNames) {
+cli.responders.listLogs = function () {
+  _logs.list(true, function (err, logFileNames) {
     if (!err && logFileNames && logFileNames.length > 0) {
       cli.verticalSpace();
-      logFileNames.forEach(function(logFileName) {
+      logFileNames.forEach(function (logFileName) {
         if (logFileName.indexOf('-') > -1) {
           console.log(logFileName);
           cli.verticalSpace();
@@ -305,18 +306,18 @@ cli.responders.listLogs = function() {
 };
 
 // More logs info
-cli.responders.moreLogInfo = function(str) {
+cli.responders.moreLogInfo = function (str) {
   // Get logFileName from string
   var arr = str.split('--');
-  var logFileName = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+  var logFileName = typeof (arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
   if (logFileName) {
     cli.verticalSpace();
     // Decompress it
-    _logs.decompress(logFileName, function(err, strData) {
+    _logs.decompress(logFileName, function (err, strData) {
       if (!err && strData) {
         // Split it into lines
         var arr = strData.split('\n');
-        arr.forEach(function(jsonString) {
+        arr.forEach(function (jsonString) {
           var logObject = helpers.parseJsonToObject(jsonString);
           if (logObject && JSON.stringify(logObject) !== '{}') {
             console.dir(logObject, {
@@ -331,8 +332,8 @@ cli.responders.moreLogInfo = function(str) {
 };
 
 // Input processor
-cli.processInput = function(str) {
-  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : false;
+cli.processInput = function (str) {
+  str = typeof (str) == 'string' && str.trim().length > 0 ? str.trim() : false;
   // Only process the input if the user actually wrote something, otherwise ignore it
   if (str) {
     // Codify the unique strings that identify the different unique questions allowed be the asked
@@ -352,7 +353,7 @@ cli.processInput = function(str) {
     // Go through the possible inputs, emit event when a match is found
     var matchFound = false;
     var counter = 0;
-    uniqueInputs.some(function(input) {
+    uniqueInputs.some(function (input) {
       if (str.toLowerCase().indexOf(input) > -1) {
         matchFound = true;
         // Emit event matching the unique input, and include the full string given
@@ -370,7 +371,7 @@ cli.processInput = function(str) {
 };
 
 // Init script
-cli.init = function() {
+cli.init = function () {
 
   // Send to console, in dark blue
   console.log('\x1b[34m%s\x1b[0m', 'The CLI is running');
@@ -386,7 +387,7 @@ cli.init = function() {
   _interface.prompt();
 
   // Handle each line of input separately
-  _interface.on('line', function(str) {
+  _interface.on('line', function (str) {
 
     // Send to the input processor
     cli.processInput(str);
@@ -396,7 +397,7 @@ cli.init = function() {
   });
 
   // If the user stops the CLI, kill the associated process
-  _interface.on('close', function() {
+  _interface.on('close', function () {
     process.exit(0);
   });
 
